9eceb0b241cdf63f7d92a5457c0f072e123987f7
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-30 22:26:38 +0300

    Split the codes

---
+++ scripts/delete-fork-repos.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+const API_URL = 'https://api.github.com';
+
+async function getForkedRepos(username, token) {
+  try {
+    let url = `${API_URL}/user/repos`;
+    const headers = {
+      'Authorization': `token ${token}`
+    };
+    const params = {
+      type: 'owner',
+      per_page: 100
+    };
+    let repos = [];
+    while (url) {
+      const response = await axios.get(url, { headers, params });
+      repos = repos.concat(response.data.filter(repo => repo.fork && !repo.private));
+      url = (response.headers.link && response.headers.link.includes('rel="next"')) 
+            ? response.headers.link.match(/<(.*?)>; rel="next"/)[1] 
+            : null;
+    }
+    return repos;
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+    return [];
+  }
+}
+
+async function deleteRepo(repoFullName, token) {
+  try {
+    const url = `${API_URL}/repos/${repoFullName}`;
+    const headers = {
+      'Authorization': `token ${token}`
+    };
+    await axios.delete(url, { headers });
+    console.log(`Deleted: ${repoFullName}`);
+  } catch (error) {
+    console.error(`Failed to delete: ${repoFullName} - ${error.response.status}`);
+  }
+}
+
+module.export = (async () => {
+  const forkedRepos = await getForkedRepos(GITHUB_USERNAME, GITHUB_TOKEN);
+  console.log(`Found ${forkedRepos.length} forked repositories.`);
+  for (const repo of forkedRepos) {
+    await deleteRepo(repo.full_name, GITHUB_TOKEN);
+  }
+})();
